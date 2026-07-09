2c5dc29d238a273ffde8527e27debd3ab1e727d6
Prakhar Singh Saini <152799995+sainiprakhar23@users.noreply.github.com>
2026-07-09 15:20:41 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (94.44%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -1,42 +1,71 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void insert(TreeNode* root, int& data){
-        if(root == nullptr){ 
-                root = new TreeNode(data);
-                return;
-        }
-        if(root->val > data){
-            if(root->left ==NULL){ 
-                root->left = new TreeNode(data); 
-                return;
+    // void insert(TreeNode* root, int& data){
+    //     if(root == nullptr){ 
+    //             root = new TreeNode(data);
+    //             return;
+    //     }
+    //     if(root->val > data){
+    //         if(root->left ==NULL){ 
+    //             root->left = new TreeNode(data); 
+    //             return;
+    //         }
+    //         insert(root->left,data);
+    //     }
+    //     if(root->val < data){
+    //         if(root->right ==NULL){ 
+    //             root->right = new TreeNode(data); 
+    //             return;
+    //         }
+    //         insert(root->right,data);
+    //     } 
+    // }
+    // TreeNode* bstFromPreorder(vector<int>& preorder) {
+    //     TreeNode* root = new TreeNode(preorder[0]);
+    //     for(int i=1;i<preorder.size();i++){
+    //         insert(root,preorder[i]);
+    //     }
+    //     return root;
+
+    // }
+
+
+
+
+    /*ITERATION*/
+    TreeNode* insert(TreeNode* root, int data) {
+        TreeNode* node = new TreeNode(data);
+
+        if (root == NULL)
+            return node;
+
+        TreeNode* curr = root;
+
+        while (true) {
+            if (data < curr->val) {
+                if (curr->left == NULL) {
+                    curr->left = node;
+                    break;
+                }
+                curr = curr->left;
             }
-            insert(root->left,data);
-        }
-        if(root->val < data){
-            if(root->right ==NULL){ 
-                root->right = new TreeNode(data); 
-                return;
+            else {
+                if (curr->right == NULL) {
+                    curr->right = node;
+                    break;
+                }
+                curr = curr->right;
             }
-            insert(root->right,data);
-        } 
+        }
+
+        return root;
     }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        TreeNode* root = new TreeNode(preorder[0]);
-        for(int i=1;i<preorder.size();i++){
-            insert(root,preorder[i]);
+        TreeNode* root = NULL;
+        for (int x : preorder) {
+            root = insert(root, x);
         }
-        return root;
 
+        return root;
     }
 };
