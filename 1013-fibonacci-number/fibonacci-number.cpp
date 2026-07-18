b302c43698907acc3e986e866e25f727759c011f
Prakhar Singh Saini <152799995+sainiprakhar23@users.noreply.github.com>
2026-07-18 23:11:46 +0530

    Time: 11 ms (37.97%) | Memory: 7.8 MB (65.24%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,17 +1,8 @@
 class Solution {
 public:
-    int fibo(int n){
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
-        return fibo(n-1)+fibo(n-2);
-    }
     int fib(int n) {
-        return fibo(n);
-
-        
+        if(n<=0) return 0;
+        if(n==1) return 1;
+        return fib(n-1)+fib(n-2);
     }
 };
