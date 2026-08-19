9ef460bf168ad7c8cfa4a81ca4ee8767e30b8447
Prakhar Singh Saini <152799995+sainiprakhar23@users.noreply.github.com>
2026-08-20 00:50:42 +0530

    Time: 3 ms (2.63%) | Memory: 11.9 MB (5.41%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -2,6 +2,32 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
         int n = nums.size();
+        // bruteforce approach
+        vector<int> sorted(n); //making a sorted array and check if it is sorted or not
+        for(int r=0;r<n;r++){
+            int idx=0;
+            for(int i=r;i<n;i++){
+                sorted[idx]=nums[i];
+                idx++;
+            }
+            // for the remaining part of array
+            for(int i=0;i<r;i++){
+                sorted[idx]=nums[i];
+                idx++;
+            }
+    
+            // check if array is sorted or not
+            bool isSorted=true;
+            for(int j=0;j<n-1;j++){
+                if(sorted[j] > sorted[j+1]){
+                    isSorted= false;
+                    break;
+                }
+            }
+
+            if(isSorted) return true;
+        }
+        return false;
 
         // brtueforce with better approach
         // vector<int> sorted = nums;
@@ -23,14 +49,14 @@ public:
 
         // optimal approach
         //check for 1 dip or 1 peak
-        int peak=0;
-        for(int i=0;i<n;i++){
-            if(nums[i] > nums[(i+1) % n]){
-                peak++;
-            }
-        }
+        // int peak=0;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i] > nums[(i+1) % n]){
+        //         peak++;
+        //     }
+        // }
 
-        if(peak<2) return true;
-        else return false;
+        // if(peak<2) return true;
+        // return false;
     }
 };
