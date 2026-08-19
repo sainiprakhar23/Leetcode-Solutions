191db7961043d31392efab866a7602ee5a9e90ea
Prakhar Singh Saini <152799995+sainiprakhar23@users.noreply.github.com>
2026-08-20 00:44:19 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (55.98%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -2,21 +2,35 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
         int n = nums.size();
-        vector<int> sorted = nums;
-        sort(nums.begin(),nums.end());
-       
-        
-        for(int r=0;r<n;r++){  //first loop is for the rotation(r=2, means rotated by 2 place)
-             bool flag=true;
-            for(int i=0;i<n;i++){ //now check if positons are correct
-                if(sorted[i] != nums[(i+r) % n]){
-                    flag=false;
-                    break;
-                }
-            }
 
-            if(flag) return flag;
+        // brtueforce with better approach
+        // vector<int> sorted = nums;
+        // sort(nums.begin(),nums.end());
+     
+        // for(int r=0;r<n;r++){  //first loop is for the rotation(r=2, means rotated by 2 place)
+        //      bool flag=true;
+        //     for(int i=0;i<n;i++){ //now check if positons are correct
+        //         if(sorted[i] != nums[(i+r) % n]){
+        //             flag=false;
+        //             break;
+        //         }
+        //     }
+
+        //     if(flag) return flag;
+        // }
+        // return false;
+
+
+        // optimal approach
+        //check for 1 dip or 1 peak
+        int peak=0;
+        for(int i=0;i<n;i++){
+            if(nums[i] > nums[(i+1) % n]){
+                peak++;
+            }
         }
-        return false;
+
+        if(peak<2) return true;
+        else return false;
     }
 };
