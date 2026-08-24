368940f76777112fbfc88e5382aa892930e3125d
Prakhar Singh Saini <152799995+sainiprakhar23@users.noreply.github.com>
2026-08-25 01:10:02 +0530

    Time: 3 ms (34.54%) | Memory: 44.7 MB (55.70%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -3,13 +3,28 @@ public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
         int n = nums.size();
         int count=0;
-        for(int i=0;i<n;i++){
-            unsigned long long product=1;
-            for(int j=i;j<n;j++){
-                product = product * nums[j];
-                if(product >= k) break;
-                count++;
+        // improved Brutefoce
+        // for(int i=0;i<n;i++){
+        //     unsigned long long product=1;
+        //     for(int j=i;j<n;j++){
+        //         product = product * nums[j];
+        //         if(product >= k) break;
+        //         count++;
+        //     }
+        // }
+
+
+        // optimal --> sliding window;
+        if(k<=1) return 0;
+        int left =0;
+        int  product = 1;
+        for(int right=0; right<n;right++){
+            product*=nums[right];
+            while(product >=k ){
+                product/=nums[left];
+                left++;
             }
+            count += right-left+1;
         }
         return count;
     }
