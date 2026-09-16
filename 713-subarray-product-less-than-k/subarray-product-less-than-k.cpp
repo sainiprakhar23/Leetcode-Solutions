e5ebe35780bd22d2819e5d9e3dd5d4471e60fa37
Prakhar Singh Saini <152799995+sainiprakhar23@users.noreply.github.com>
2026-09-17 02:00:02 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (74.52%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        int n = nums.size();
-        int count=0;
-        // improved Brutefoce
-        // for(int i=0;i<n;i++){
-        //     unsigned long long product=1;
-        //     for(int j=i;j<n;j++){
-        //         product = product * nums[j];
-        //         if(product >= k) break;
-        //         count++;
-        //     }
-        // }
-
-
-        // optimal --> sliding window;
         if(k<=1) return 0;
-        int left =0;
-        int  product = 1;
-        for(int right=0; right<n;right++){
+        int n = nums.size();
+        int product = 1;
+        int count=0;
+        int left=0;
+        for(int right=0;right<n;right++){
             product*=nums[right];
-            while(product >=k ){
-                product/=nums[left];
+            while(product>=k){
+                product = product/nums[left];
                 left++;
             }
             count += right-left+1;
