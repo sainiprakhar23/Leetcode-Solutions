24511d7afa21a92617bfe73b7be8188d3f7ba541
Prakhar Singh Saini <152799995+sainiprakhar23@users.noreply.github.com>
2026-09-17 13:08:10 +0530

    Time: 0 ms (100.00%) | Memory: 69.8 MB (5.29%) - LeetSync

---
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int n  = nums.size();
+
+        // BRUTEFORCE
+        // int count=0;
+        // int zeroCount=0;
+        // for(int i=0;i<n;i++){
+        //     zeroCount=0;
+        //     for(int j=i;j<n;j++){
+        //         if(nums[j]==0) zeroCount++;
+        //         if(zeroCount > k){
+        //             break;
+        //         }
+        //         count = max(count, j-i+1);
+        //     }
+ 
+        // }
+        // return count;
+
+        //OPTIMISED THEN BRUTEFORCE LESS THAN O(n2)
+        int max_Ans=0;
+        int zeroCount=0;
+        int left=0;
+        for(int right=0;right<n;right++){
+            if(nums[right]==0) zeroCount++;
+
+            while(zeroCount > k){
+                if(nums[left]==0) zeroCount--;
+                left++;
+            }
+            max_Ans=max(max_Ans,right-left+1);
+        }
+        return max_Ans;
+    }
+};
