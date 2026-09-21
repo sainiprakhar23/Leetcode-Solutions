be526acff2d93e407d6394ff40ee8b0b555b0eb7
Prakhar Singh Saini <152799995+sainiprakhar23@users.noreply.github.com>
2026-09-21 19:53:31 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (68.25%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
@@ -1,29 +1,16 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        ListNode* currNode = head;
-        while(currNode != NULL &&currNode->next != NULL){
-            if(currNode->val == currNode->next->val){
-                ListNode* temp = currNode->next;
-                currNode->next = temp->next;
+        ListNode* currentNode = head;
+        while(currentNode != NULL && currentNode->next!=NULL){
+            if(currentNode->val == currentNode->next->val){
+                ListNode* temp = currentNode->next;
+                currentNode->next = temp->next;
                 delete temp;
             }
             else{
-                // in else becz after deleting dulpicate we should on the same node ,
-                // as another dupicate may be there, we will move the curr->next has diff value
-             currNode = currNode->next;      
+                currentNode = currentNode->next;
             }
-            
         }
         return head;
     }
