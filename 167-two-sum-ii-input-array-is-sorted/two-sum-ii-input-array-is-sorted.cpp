57d5545b48840ea6c4d749b6c0b9ff71405f997d
Prakhar Singh Saini <152799995+sainiprakhar23@users.noreply.github.com>
2026-09-21 19:25:03 +0530

    Time: 0 ms (100.00%) | Memory: 25.7 MB (5.40%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        // using 2 pointer approach
-        // int i = 0;
-        // int j = numbers.size()-1;
-        // while(i<j){
-        //     int sum = numbers[i]+numbers[j];
-        //     if(sum == target) return {i+1,j+1};
-        //     if(sum < target) i++;
-        //     else j--; //(sum > target)
-        // }
-        // return {};
-
-        // using map
-        unordered_map<int,int> mp;
-        for(int i=0;i<numbers.size();i++){
-            int remain = target - numbers[i];
-            if(mp.find(remain) != mp.end()){
-                return {mp[remain]+1,i+1};
+        int n = numbers.size();
+        //  TWO POINTER APPROACH
+        int left = 0;
+        int right = n-1;
+        while(left<right){
+            int sum = numbers[left] + numbers[right];
+            if(sum > target) right--;
+            else  if(sum < target) left++;
+            else{
+                // sum == target
+                return {left+1, right+1};
             }
-            mp[numbers[i]]=i;
-            
         }
         return {};
 
 
+        // USING HASHMAP
+        // unordered_map<int,int> mp;
+        // for(int i=0;i<n;i++){
+        //     int needed = target-nums[i];
+        //     if(mp.find(needed) != mp.end()){
+        //         return {i+1,mp[needed]+1};
+        //         break;
+        //     }
+        //     // if not found in map, stor it in the map
+        //     mp[nums[i]]=i;
+        // }
+        // return {};
+
+        
     }
 };
