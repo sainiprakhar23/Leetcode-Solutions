c8fda074704773489bc4468379941182dbae7fad
Prakhar Singh Saini <152799995+sainiprakhar23@users.noreply.github.com>
2026-09-24 18:54:41 +0530

    Time: 2 ms (21.74%) | Memory: 35.8 MB (27.91%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -3,37 +3,37 @@ public:
     int pivotIndex(vector<int>& nums) {
         int n = nums.size();
         // using prefix and suffix sum array
-        vector<int> prefix(n,0);
-        vector<int> suffix(n,0);
-        prefix[0]=0;
-        suffix[n-1]=0;
-        for(int i=1;i<n;i++){
-            prefix[i] = prefix[i-1] + nums[i-1];
-        }
-        for(int j=n-2;j>=0;j--){
-            suffix[j] = suffix[j+1] + nums[j+1];
-        }
-        for(int k=0;k<n;k++){
-            if(prefix[k]==suffix[k]){
-                return k;
-            }
-        }
-        return -1;
-
-        // using total sum and left variable
-        // int totalSum=0;
-        // int left = 0;
-        // int right = 0;
-        // for(int i=0;i<n;i++){
-        //     totalSum+=nums[i];
+        // vector<int> prefix(n,0);
+        // vector<int> suffix(n,0);
+        // prefix[0]=0;
+        // suffix[n-1]=0;
+        // for(int i=1;i<n;i++){
+        //     prefix[i] = prefix[i-1] + nums[i-1];
         // }
-        
-        // for(int j=1;j<n;j++){
-        //     left += numsji-1];
-        //     right = totalSum - nums[j] - left;
-        //     if(left==right) return j;
+        // for(int j=n-2;j>=0;j--){
+        //     suffix[j] = suffix[j+1] + nums[j+1];
+        // }
+        // for(int k=0;k<n;k++){
+        //     if(prefix[k]==suffix[k]){
+        //         return k;
+        //     }
         // }
         // return -1;
+
+        // using total sum and left variable
+        int totalSum=0;
+        int left = 0;
+        int right = 0;
+        for(int i=0;i<n;i++){
+            totalSum+=nums[i];
+        }
+        
+        for(int j=0;j<n;j++){
+            if(j!=0) left += nums[j-1];
+            right = totalSum - nums[j] - left;
+            if(left==right) return j;
+        }
+        return -1;
 
     }
 };
