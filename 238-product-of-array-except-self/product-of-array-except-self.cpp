af7c94db40abb9822c0f2350758b5ac748125949
Prakhar Singh Saini <152799995+sainiprakhar23@users.noreply.github.com>
2026-09-26 23:52:27 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (95.42%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -16,21 +16,35 @@ public:
 
 
         // OPTIMAL -> PREFIX SUM
-        vector<int>prefixProduct(n);
-        vector<int>suffixProduct(n);
+        // vector<int>prefixProduct(n);
+        // vector<int>suffixProduct(n);
         vector<int>result(n);
-        prefixProduct[0]=1;
-        suffixProduct[n-1]=1;
+        // prefixProduct[0]=1;
+        // suffixProduct[n-1]=1;
+        // for(int i=1;i<n;i++){
+        //     prefixProduct[i] = nums[i-1] * prefixProduct[i-1];
+        // }
+        // for(int j=n-2;j>=0;j--){
+        //     suffixProduct[j] = nums[j+1] * suffixProduct[j+1];
+        // }
+        // for(int k=0;k<n;k++){
+        //     result[k] = prefixProduct[k] * suffixProduct[k]; 
+        // }
+
+        // return result;
+
+        // truing to solve folow up??
+        result[0]=1;
         for(int i=1;i<n;i++){
-            prefixProduct[i] = nums[i-1] * prefixProduct[i-1];
-        }
-        for(int j=n-2;j>=0;j--){
-            suffixProduct[j] = nums[j+1] * suffixProduct[j+1];
-        }
-        for(int k=0;k<n;k++){
-            result[k] = prefixProduct[k] * suffixProduct[k]; 
+            result[i] = result[i-1] * nums[i-1];
         }
 
+        // now maintain a right varible
+        int rightProduct = 1;
+        for(int j = n-2;j>=0;j--){
+            rightProduct = rightProduct * nums[j+1];
+            result[j] = result[j] * rightProduct;
+        }
         return result;
     }
 };
